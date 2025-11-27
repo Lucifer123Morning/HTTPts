7dc851f6d1edfa12087d626c5a0608fc21b3165c
Lucifer Listopadov <listopadov2744@gmail.com>
2025-11-27 13:48:04 -0800

    Initial commit

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,12 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-// Нужно, чтобы корректно работать с путями в ES-модулях
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 8080;
-// Раздаём статические файлы из папки "assets"
-// URL будет /assets/logo.png
-app.use("/assets", express.static(path.join(__dirname, "../assets")));
+function handlerReadiness(_req, res) {
+    res.set("Content-Type", "text/plain");
+    res.send("OK");
+}
+app.use("/app", express.static("./src/app"));
+app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
